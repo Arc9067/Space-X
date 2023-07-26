4c7b1c0a3068faa4e86f14e37d82b17a2017003a
Arc <atandarokib@gmail.com>
2023-07-26 21:55:49 +0100

    up

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="container  w-full relative z-10">
         <div className="h-full px-10 py-20 lg:py-36 gap-20 lg:border-l border-slate-200 w-full grid grid-cols-1 lg:grid-cols-2 justify-center items-center">
           <h1 className="text-cyan-400 uppercase text-5xl md:text-8xl text-center lg:text-start font-normal ">
-            Operation Mission Moon
+            Operation Moon Mission
           </h1>
           <img src={space} alt="" className="relative -top-14" />
         </div>
